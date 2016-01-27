76872791d880a2685f211623f85d809e451b7ba6
Owen <owen.wu@nuance.com>
2016-01-27 13:37:07 +0800

    Major

--- app/src/main/java/com/example/junxia/mynmtapplication/MainActivity.java
+++ app/src/main/java/com/example/junxia/mynmtapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.junxia.mynmtapplication;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.NavigationView;
@@ -10,12 +12,15 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private WorkerThread mWorkerThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +47,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Log.i(VoconConst.GRAMMAR_NAME, "print DROP_LENGTH");
     }
 
     @Override
@@ -58,6 +65,10 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        
+        mWorkerThread = new WorkerThread("audioSaving-Thread");
+        mWorkerThread.start();
+
         return true;
     }
 
