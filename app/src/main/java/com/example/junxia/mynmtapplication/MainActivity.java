908efc0d5b497588c716f4b56fd5d7f401903ee6
Owen <owen.wu@nuance.com>
2016-01-27 13:38:24 +0800

    remove nfc

--- app/src/main/java/com/example/junxia/mynmtapplication/MainActivity.java
+++ app/src/main/java/com/example/junxia/mynmtapplication/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.junxia.mynmtapplication;
 
-import android.nfc.Tag;
 import android.os.Bundle;
 
 import android.support.design.widget.FloatingActionButton;
@@ -65,7 +64,7 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-        
+
         mWorkerThread = new WorkerThread("audioSaving-Thread");
         mWorkerThread.start();
 
